0954a325d380e6bd79786cf445b72f4f01c50974
seeratawan01 <seeratsdsking@gmail.com>
2022-11-06 01:37:34 +0500

    fix: esm export

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
 import { DOMEvent, JSON, PluginBuilder } from './core'
 import { prepareEventPayload, shouldCaptureEvent } from './helpers'
-import { AutoCapture as AutoCaptureDefault } from './autoCapture'
-import {TapPlugin, VideoPlugin, SwipePlugin, PageViewPlugin, MouseMovementPlugin, ScrollPlugin , FormPlugin} from './plugins'
-
-// Auto register all internal plugin
-AutoCaptureDefault.use(new TapPlugin())
-AutoCaptureDefault.use(new FormPlugin())
-AutoCaptureDefault.use(new VideoPlugin())
-AutoCaptureDefault.use(new SwipePlugin())
-AutoCaptureDefault.use(new PageViewPlugin())
-AutoCaptureDefault.use(new MouseMovementPlugin())
-AutoCaptureDefault.use(new ScrollPlugin())
+import { AutoCapture } from './autoCapture'
 
 // exporting the default instance with all the plugins
-export default AutoCaptureDefault;
-
-
-// exporting the core classes for minimal bundle size and tree shaking
-export { AutoCapture } from './autoCapture'
+export default AutoCapture;
 
 // exporting the built-in plugins for the developer to use them
 export * from './plugins'
--- src/index.umd.ts
+++ src/index.umd.ts
@@ -1,3 +1,18 @@
-import AutoCapture from '.'
+import AutoCapture, {
+  FormPlugin, MouseMovementPlugin,
+  PageViewPlugin, ScrollPlugin,
+  SwipePlugin,
+  TapPlugin,
+  VideoPlugin
+} from '.'
+
+// Auto register all internal plugin
+AutoCapture.use(new TapPlugin())
+AutoCapture.use(new FormPlugin())
+AutoCapture.use(new VideoPlugin())
+AutoCapture.use(new SwipePlugin())
+AutoCapture.use(new PageViewPlugin())
+AutoCapture.use(new MouseMovementPlugin())
+AutoCapture.use(new ScrollPlugin())
 
 export default AutoCapture
